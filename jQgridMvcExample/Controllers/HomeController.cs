547a01d3881a0a1f78783c6d801ec728cbdd5bf0
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-10-01 20:32:59 +0530

    jqgrid example inline editing in mvc

---
+++ jQgridMvcExample/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using jQgridMvcExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQgridMvcExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddEducation(EducationalDetails rowData)
+        {
+
+            return Content("Save");
+        }
+    }
+}
---
+++ jQgridMvcExample/Models/EducationalDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jQgridMvcExample.Models
+{
+    public class EducationalDetails
+    {
+        public string act { get; set; }
+        public string continent { get; set; }
+        public string country { get; set; }
+        public string name { get; set; }
+        public string stage { get; set; }
+
+    }
+}
